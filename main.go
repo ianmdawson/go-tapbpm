7fc0e384136e54e117a505f962036b2c74b2e064
Ian M. Dawson <ianmdawson@gmail.com>
2019-05-26 14:27:59 -0700

    move taptracker struct to its own file

--- main.go
+++ main.go
@@ -2,44 +2,10 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"time"
 
 	"github.com/eiannone/keyboard"
 )
-
-type tapTracker struct {
-	trackedTime  *time.Time // TODO: rename this previous tap time?
-	totalTime    time.Duration
-	numberOfTaps int
-}
-
-func (trkr *tapTracker) reset() {
-	trkr.totalTime = time.Nanosecond - 1 // TODO: is there a cleaner way to do this?
-	trkr.numberOfTaps = 0
-	trkr.trackedTime = nil
-}
-
-func (trkr *tapTracker) tap(newTime time.Time) {
-	prevTime := trkr.trackedTime
-	trkr.trackedTime = &newTime
-	if prevTime != nil {
-		trkr.totalTime = trkr.totalTime + newTime.Sub(*prevTime)
-	}
-	trkr.numberOfTaps++
-}
-
-func (trkr *tapTracker) bpm() float64 {
-	if trkr.trackedTime == nil {
-		return float64(0)
-	}
-	bpm := (float64(trkr.numberOfTaps) / trkr.totalTime.Minutes())
-	return bpm
-}
-
-func (trkr *tapTracker) bpmString() string {
-	return strconv.FormatFloat(trkr.bpm(), 'f', 2, 64)
-}
 
 func inputIsQuitKey(char rune, key keyboard.Key) bool {
 	if string(char) == "q" || key == keyboard.KeyCtrlC || key == keyboard.KeyEsc {
---
+++ taptracker.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"strconv"
+	"time"
+)
+
+type tapTracker struct {
+	trackedTime  *time.Time // TODO: rename this previous tap time?
+	totalTime    time.Duration
+	numberOfTaps int
+}
+
+func (trkr *tapTracker) reset() {
+	trkr.totalTime = time.Nanosecond - 1 // TODO: is there a cleaner way to do this?
+	trkr.numberOfTaps = 0
+	trkr.trackedTime = nil
+}
+
+func (trkr *tapTracker) tap(newTime time.Time) {
+	prevTime := trkr.trackedTime
+	trkr.trackedTime = &newTime
+	if prevTime != nil {
+		trkr.totalTime = trkr.totalTime + newTime.Sub(*prevTime)
+	}
+	trkr.numberOfTaps++
+}
+
+func (trkr *tapTracker) bpm() float64 {
+	if trkr.trackedTime == nil {
+		return float64(0)
+	}
+	bpm := (float64(trkr.numberOfTaps) / trkr.totalTime.Minutes())
+	return bpm
+}
+
+func (trkr *tapTracker) bpmString() string {
+	return strconv.FormatFloat(trkr.bpm(), 'f', 2, 64)
+}
